5342b4fdf0ad1c8e330399e0a2bf0a726fbe88ee
oaf-tobark <57107859+oaf-tobark@users.noreply.github.com>
2021-03-27 15:25:18 -0600

    Update Initials.py

--- Initials.py
+++ Initials.py
@@ -1,1 +1,31 @@
+# My name is Jason Landers
+# I am 6'5"" tall!
 
+initials_j = """ 
+               J J J J J
+                   J
+                   J
+                   J
+               J   J
+               J   J
+                J J 
+             """
+initials_m = """
+              M       M
+              M M   M M
+              M M   M M
+              M   M   M
+              M       M
+              M       M
+              M       M
+             """
+initials_l = """
+             L
+             L
+             L
+             L
+             L
+             L
+             L L L L L
+             """
+print (initials_j, initials_m, initials_l)
